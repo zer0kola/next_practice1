2e4f97f07c566aa91cb2907c742bb05b6f36c3a3
zer0kola <zer0kola321@gmail.com>
2023-04-29 15:05:43 +0900

    dynamic routing (notFound)

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,8 +1,22 @@
+import { notFound } from "next/navigation";
+
 type Props = {
   params: {
     slug: string;
   };
 };
+
 export default function ProductPage({ params }: Props) {
+  if (params.slug === "nothing") {
+    notFound();
+  }
+
   return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
