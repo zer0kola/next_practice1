55e237947ce06390c29fe7e4800a1765bcc6ddee
zer0kola <zer0kola321@gmail.com>
2023-04-30 15:20:58 +0900

    SEO (Metadata)

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,6 +6,7 @@ export default function ProductsPage() {
     <div>
       <h1>제품 소개 페이지</h1>
       <ul>
+        {/* products 배열의 각 요소를 순회하면서 Link 컴포넌트를 생성 */}
         {products.map((product, index) => (
           <li key={index}>
             <Link href={`/products/${product}`}>{product}</Link>
